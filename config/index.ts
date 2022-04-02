07f44d2060dd78ed8e6c37b20aed91f58860429a
Michał Woźniak <lachimek1@gmail.com>
2022-04-03 00:08:09 +0200

    fetch_url test for prod

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://kntp.pl'
+export const server = dev
+  ? 'http://localhost:3000'
+  : 'https://kntp-links-1texc8ksh-lachimek.vercel.app'

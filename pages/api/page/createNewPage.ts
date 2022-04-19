c760c0bdf94466ccd63d19bd41a6a1390900bb94
Michał Woźniak <lachimek1@gmail.com>
2022-04-19 22:03:35 +0200

    cloudinary image upload

--- pages/api/page/createNewPage.ts
+++ pages/api/page/createNewPage.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const formidable = require('formidable-serverless')
 import { CID, create } from 'ipfs-http-client'
 import fs from 'fs'
+import { v2 as cloudinary } from 'cloudinary'
 import prisma from 'db'
 import path from 'path'
 import { Fields } from 'formidable'
@@ -18,10 +19,6 @@ interface PageData {
   pageUrl: string
 }
 
-interface TrackNextApiRequest extends NextApiRequest {
-  body: PageData
-}
-
 export const config = {
   api: {
     externalResolver: true,
@@ -34,16 +31,6 @@ export default async function handler(
   res: NextApiResponse<ErrorResponse>
 ) {
   if (req.method === 'POST') {
-    let x = process.env.INFURA_PROJECT_ID
-    let y = process.env.INFURA_PROJECT_SECRET
-    let b64auth = Buffer.from(`${x}:${y}`).toString('base64')
-    const client = create({
-      url: 'https://ipfs.infura.io:5001/api/v0',
-      headers: {
-        Authorization: `Basic ${b64auth}`,
-      },
-    })
-
     const form = new formidable.IncomingForm()
     form.keepExtensions = true
 
@@ -53,52 +40,42 @@ export default async function handler(
           where: { pageName: fields.pageUrl },
         })
         if (!pages) {
-          try {
-            //const data = fs.readFileSync(files.file.path)
-            //const { cid } = await client.add(data)
-            //const imgPath = cid.toV1().toString()
-            const imgPath = 'testest'
-            const url = `https://${imgPath}.ipfs.infura-ipfs.io`
-            //if (imgPath !== '') {
-            if (true) {
-              const user = await prisma.user.update({
-                where: { email: fields.userEmail },
-                include: { pages: true },
-                data: {
-                  pages: {
-                    create: [
-                      {
-                        pageName: fields.pageUrl,
-                        profilePictureLink: url,
-                        userName: fields.pageName,
-                        description: fields.pageDescription,
-                      },
-                    ],
+          const data = fs.createReadStream(files.file.path)
+          let uploadStream = cloudinary.uploader.upload_stream(
+            { format: 'png', folder: 'user_images' },
+            async (error, result) => {
+              if (!error && result) {
+                const user = await prisma.user.update({
+                  where: { email: fields.userEmail },
+                  include: { pages: true },
+                  data: {
+                    pages: {
+                      create: [
+                        {
+                          pageName: fields.pageUrl,
+                          profilePictureLink: result.url,
+                          userName: fields.pageName,
+                          description: fields.pageDescription,
+                        },
+                      ],
+                    },
                   },
-                },
-              })
-            } else {
-              res.status(200).json({ error: true, message: 'page_url_exists' })
+                })
+                res.status(200).json({ error: false, message: 'ok' })
+              } else {
+                console.error(error)
+                res.status(200).json({ error: true, message: 'upload_error' })
+              }
             }
-          } catch (err) {
-            console.error(err)
-            res.status(200).json({ error: true, message: 'ipfs_error' })
-          }
+          )
+          data.pipe(uploadStream)
+        } else {
+          res.status(200).json({ error: true, message: 'page_url_exists' })
         }
-      }
-
-      if (err) {
-        return res.status(400).json({ error: true, message: err })
+      } else {
+        res.status(200).json({ error: true, message: 'no_page_url_provided' })
       }
     })
-
-    res.status(200).json({ error: false, message: 'ok' })
-
-    // if (!found) {
-    //   res.status(200).json({ error: false, message: 'ok' })
-    // } else {
-    //   res.status(200).json({ error: true, message: 'page_url_exists' })
-    // }
   } else {
     res.status(200).json({ error: true, message: 'Halo halo!' })
   }

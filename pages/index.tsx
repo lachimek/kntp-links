de0b530985ed212ff7c6d91dd4b0d312f8f3917a
Michał Woźniak <lachimek1@gmail.com>
2022-06-12 14:05:25 +0200

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,51 @@
 import { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/react'
+import { getSession, signIn } from 'next-auth/react'
 import { Page } from 'types/page'
+
+const items = ['Youtube', 'Facebook', 'Twitch']
 
 const Home: Page = () => {
   return (
-    <div className="flex w-full justify-between px-8 pt-4">
-      <span>Strona domowa</span>
+    <div className="flex w-full justify-center px-8 pt-4">
+      <div className="flex flex-col items-center">
+        <div className="flex h-fit flex-col text-3xl font-extrabold leading-normal sm:flex-row sm:text-5xl sm:leading-normal md:text-6xl md:leading-normal">
+          <span className="pb-4">Twój link do</span>
+          <span className="relative ml-4 inline-flex overflow-y-hidden font-extrabold">
+            <span className="select-none text-transparent" aria-hidden="true">
+              Instagram
+            </span>
+            <span className="absolute top-0 left-0 animate-rotate-text text-center">
+              {/* animate-rotate-text */}
+              <span className="block text-[#FF0000]">YouTube</span>
+              <span className="block text-blue-500">Facebook</span>
+              <span className="h-fit bg-gradient-to-br from-[#405de6] via-[#833ab4] to-[#fd1d1d] bg-clip-text text-transparent">
+                Instagram
+              </span>
+              <span className="block text-purple-500">Twitch</span>
+              <span className="block text-black">GitHub</span>
+              <span className="block text-[#1DA1F2]">Twitter</span>
+              <span className="tiktok-drop-shadow block text-black">
+                TikTok
+              </span>
+              <span className="block text-[#FF0000]">YouTube</span>
+            </span>
+          </span>
+        </div>
+        <div className="mt-8 flex w-full flex-col">
+          <span className="text-center text-3xl font-extrabold sm:text-5xl">
+            Wszystko w jednym miejscu.
+          </span>
+          <div className="mt-4 flex flex-col items-center text-3xl font-extrabold sm:text-5xl lg:flex-row">
+            <span
+              className="my-4 cursor-pointer rounded-md px-2 pb-2 transition-all before:content-['→'] after:content-['←'] hover:bg-white hover:text-black lg:before:content-none lg:after:content-none"
+              onClick={() => signIn()}
+            >
+              Zaloguj się
+            </span>{' '}
+            <span className="pb-2 text-center">i utwórz swoją stronę.</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ module.exports = {
     extend: {
       colors: {
         'night': "#2F2B3F"
+      },
+      dropShadow: {
+        'tiktok': [
+          '0 -1.5px -1.5px rgba(37, 244, 238, 1)',
+          '0 1.5px 1.5px rgba(254, 44, 85, 1)'
+        ]
+      },
+      animation: {
+        'rotate-text': 'cycle 12s infinite'
+      },
+      keyframes: {
+        cycle: {
+          '0%': {transform: 'translateY(-87.5%)'},
+          '14%': {transform: 'translateY(-75%)'},
+          '28%': {transform: 'translateY(-62.5%)'},
+          '42%': {transform: 'translateY(-50%)'},
+          '56%': {transform: 'translateY(-37.5%)'},
+          '70%': {transform: 'translateY(-25%)'},
+          '84%': {transform: 'translateY(-12.5%)'},
+          '100%': {transform: 'translateY(0%)'}
+        }
       }
     },
   },
